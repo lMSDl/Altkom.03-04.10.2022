ac46aed4c4c6b21f66992b41d31ae047faf49e22
Paweł Łęczycki <pawel@msd.info.pl>
2022-10-03 12:21:53 +0200

    typy numeryczne

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,66 +2,122 @@
 using System.Globalization;
 
 
-//deklaracja zmiennej lokalnej o typie string i nazwie hello
-string hello;
-
-//inicjalizacja zmiennej - pierwsze przypisanie wartości
-hello = "Hello";
-
-//delaracja z inicjalizacją
-string toWhom = "World";
-
-Console.WriteLine(hello);
-Console.WriteLine(toWhom);
-
-//przypisanie wartości
-toWhom = "class";
-
-Console.Write(hello);
-Console.Write(" ");
-Console.Write(toWhom);
-Console.WriteLine();
-
-toWhom = Console.ReadLine();
-
-//łączenie stringów
-string outputString = hello + " " + toWhom;
-outputString = outputString.ToUpper();
-Console.WriteLine(outputString);
-
-//łączenie za pomocą string.Format
-outputString = string.Format("{0} {1}!", hello, toWhom);
-Console.WriteLine(outputString);
-
-outputString = string.Format("{1} {0}!", hello, toWhom);
-Console.WriteLine(outputString);
-
-//string interpolowany
-outputString = $"{hello} {toWhom}!";
-Console.WriteLine(outputString);
-
-Console.WriteLine($"Your name has {toWhom.Length} letters");
-
-outputString = "     Hello World!   ";
-
-Console.WriteLine($"[{outputString.TrimEnd()}]");
-Console.WriteLine($"[{outputString.TrimStart()}]");
-Console.WriteLine($"[{outputString.Trim()}]");
-
-outputString = "Hello World!!";
-Console.WriteLine($"{outputString.Trim('a')}");
+int a = 20;
+int b = 8;
+int c = a + b;
+Console.WriteLine(c);
+c = a - b;
+Console.WriteLine(c);
+c = a * b;
+Console.WriteLine(c);
+c = a / b;
+Console.WriteLine(c);
+//reszta z dzielenia
+c = a % b;
+Console.WriteLine(c);
 
 
-outputString = "Hello World!!";
-Console.WriteLine($"{outputString.Contains("!!")}");
-outputString = "Hello World! How do you do, World?";
-Console.WriteLine($"{outputString.Contains("!!")}");
+c = a + a * a;
+Console.WriteLine(c);
+c = (a + a) * a;
+Console.WriteLine(c);
 
-//zastąpienie części ciągu znaków czułe na wielkość liter
-outputString = outputString.Replace("World", toWhom);
-Console.WriteLine(outputString);
 
-//włączamy ignoreCase - podmianka niezależnie od wielkości liter
-outputString = outputString.Replace("world", toWhom, true, CultureInfo.CurrentCulture);
-Console.WriteLine(outputString);
+float aa = 20f;
+float bb = 8f;
+float cc = aa / bb;
+Console.WriteLine(cc);
+cc = a / bb;
+Console.WriteLine(cc);
+
+Console.WriteLine(5/3);
+Console.WriteLine(5/3f);
+
+string stringA = a.ToString();
+string stringB = b.ToString();
+Console.WriteLine(stringA + stringB);
+
+Console.WriteLine($"short max:{short.MaxValue} min:{short.MinValue}");
+Console.WriteLine($"int max:{int.MaxValue} min:{int.MinValue}");
+Console.WriteLine($"long max:{long.MaxValue} min:{long.MinValue}");
+Console.WriteLine($"float max:{float.MaxValue} min:{float.MinValue}");
+Console.WriteLine($"double max:{double.MaxValue} min:{double.MinValue}");
+Console.WriteLine($"decimal max:{decimal.MaxValue} min:{decimal.MinValue}");
+
+
+Console.WriteLine(5 / 3.0f);
+//double
+Console.WriteLine(5 / 3.0);
+Console.WriteLine(500000000 / 3.0d);
+//decimal
+Console.WriteLine(5 / 3m);
+Console.WriteLine(500000000 / 3.0m);
+
+
+void StringsDemo()
+{
+    //deklaracja zmiennej lokalnej o typie string i nazwie hello
+    string hello;
+
+    //inicjalizacja zmiennej - pierwsze przypisanie wartości
+    hello = "Hello";
+
+    //delaracja z inicjalizacją
+    string toWhom = "World";
+
+    Console.WriteLine(hello);
+    Console.WriteLine(toWhom);
+
+    //przypisanie wartości
+    toWhom = "class";
+
+    Console.Write(hello);
+    Console.Write(" ");
+    Console.Write(toWhom);
+    Console.WriteLine();
+
+    toWhom = Console.ReadLine();
+
+    //łączenie stringów
+    string outputString = hello + " " + toWhom;
+    outputString = outputString.ToUpper();
+    Console.WriteLine(outputString);
+
+    //łączenie za pomocą string.Format
+    outputString = string.Format("{0} {1}!", hello, toWhom);
+    Console.WriteLine(outputString);
+
+    outputString = string.Format("{1} {0}!", hello, toWhom);
+    Console.WriteLine(outputString);
+
+    //string interpolowany
+    outputString = $"{hello} {toWhom}!";
+    Console.WriteLine(outputString);
+
+    Console.WriteLine($"Your name has {toWhom.Length} letters");
+
+    outputString = "     Hello World!   ";
+
+    Console.WriteLine($"[{outputString.TrimEnd()}]");
+    Console.WriteLine($"[{outputString.TrimStart()}]");
+    Console.WriteLine($"[{outputString.Trim()}]");
+
+    outputString = "Hello World!!";
+    Console.WriteLine($"{outputString.Trim('a')}");
+
+
+    outputString = "Hello World!!";
+    Console.WriteLine($"{outputString.Contains("!!")}");
+    outputString = "Hello World! How do you do, World?";
+    Console.WriteLine($"{outputString.Contains("!!")}");
+
+    //zastąpienie części ciągu znaków czułe na wielkość liter
+    outputString = outputString.Replace("World", toWhom);
+    Console.WriteLine(outputString);
+
+    //włączamy ignoreCase - podmianka niezależnie od wielkości liter
+    outputString = outputString.Replace("world", toWhom, true, CultureInfo.CurrentCulture);
+    Console.WriteLine(outputString);
+}
+
 
