befcc39bed76b80ff0ca85ba2dd87e5211a2c1a4
Paweł Łęczycki <pawel@msd.info.pl>
2022-10-04 15:55:30 +0200

    SOLID

---
+++ SOLID/Program.cs
@@ -0,0 +1,17 @@
+﻿using SOLID.L;
+
+int a = 5;
+int b = 3;
+
+Rectangle rectangle = new Square();
+SetRectangle(rectangle, a, b);
+
+Console.WriteLine($"Pole prostokąta o bokach {a} i {b} = {rectangle.GetArea()}");
+
+
+
+static void SetRectangle(Rectangle rectangle, int a, int b)
+{
+    rectangle.A = a;
+    rectangle.B = b;
+}
